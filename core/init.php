67a6c776f6906466ad62a680d7c723117fe9c046
jqn <joaquin_guardado@hotmail.com>
2015-01-09 08:52:38 -0700

    adding database setup

--- core/init.php
+++ core/init.php
@@ -3,6 +3,6 @@
   //turns off error reporting
   error_reporting(0);
 
-  
+
   require 'database/connect.php';
 ?>
---
+++ register.php
@@ -0,0 +1,11 @@
+<?php include 'core/init.php'; ?>
+<?php include 'includes/overall/header.php'; ?>
+<h1>Register</h1>
+<p>Registration page</p>
+
+<?php
+
+
+
+include 'includes/overall/footer.php';
+?>
