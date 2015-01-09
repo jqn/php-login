2177d853793a620ba5cdb0b270fb813ed8aeb818
jqn <joaquin_guardado@hotmail.com>
2015-01-09 09:19:42 -0700

    data is being saved to database

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,5 +1,5 @@
 <?php
-
-  mysql_connect('localhost', 'root', 'root');
-  mysql_select_db(login);
+  $connect_error = 'Sorry, we\'re experiencing connection issues.';
+  mysql_connect('localhost', 'root', 'root') or die($connect_error);
+  mysql_select_db(login) or die($connect_error);
 ?>
